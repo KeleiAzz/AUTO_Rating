ef2a03073392b83249f297a222583225f85e75cb
KeleiAzz <kgong@ncsu.edu>
2015-06-22 16:13:45 -0400

    Add function to execute query, return the result table

--- TextExtraction/databaseConnector.py
+++ TextExtraction/databaseConnector.py
@@ -1,1 +1,25 @@
 __author__ = 'keleigong'
+
+import MySQLdb
+
+
+def executeQuery(sql):
+    # Open database connection
+    db = MySQLdb.connect("localhost", "root", "root", "ml")
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    try:
+        # Execute the SQL command
+        cursor.execute(sql)
+        # Fetch all the rows in a list of lists.
+        results = cursor.fetchall()
+        db.close()
+        return results
+    except:
+        print('Error: Unable to fetch data')
+    # disconnect from server
+    db.close()
+
+
