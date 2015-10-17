03a6139bb2149fad8f8535c284b556ab4ef0ef8e
Kelei Gong <kgong@ncsu.edu>
2015-10-17 13:17:18 -0400

    add classes SearchQuery and SearchResult

--- URLExtraction/URLsResultsProcessing.py
+++ URLExtraction/URLsResultsProcessing.py
@@ -3,7 +3,44 @@ __author__ = 'keleigong'
 from openpyxl import load_workbook
 from openpyxl import Workbook
 import operator
+import json
 
+class SearchQuery(object):
+    def __init__(self, company, query):
+        self.company = company
+        self.keyword = ' '.join(query['query'].split(' ')[len(company.split(' ')):])
+        self.id = query['id']
+        self.results = []
+        for result in query['results']:
+            if result['link_type'] == 'results' and result['link'][0:4] == 'http':
+                self.results.append(SearchResult(company, result))
+
+class SearchResult(object):
+    def __init__(self, company, result):
+        self.company = company
+        self.domain = result['domain']
+        self.id = result['id']
+        self.link = result['link']
+        self.link_type = result['link_type']
+        self.rank = result['rank']
+        self.serp_id = result['serp_id']
+        self.snippet = result['snippet']
+        self.title = result['title']
+        self.visible_link = result['visible_link']
+        self.count = 1
+
+    def set_link(self, link):
+        self.link = link
+
+    def __eq__(self, other):
+        # print(self.link, other.link)
+        if (isinstance(other, self.__class__)) and self.link == other.link:
+            other.count += 1
+            return True
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 def urls_processing(filename):
     '''
@@ -105,6 +142,63 @@ def write_to_excel(urls, filename):
     wb2.save(filename)
     return filename
 
+def get_company_names(file_path):
+    file = open(file_path, 'r')
+    companies_names = file.read()
+    file.close()
+    companies_names = companies_names.split('\n')
+    for i in range(len(companies_names)):
+        company_splited = companies_names[i].split(' ')
+        if company_splited[-1] in ["INC"]:
+            companies_names[i] = ' '.join(company_splited[0:-1])
+            # print(companies_names[i])
+    return companies_names
+
+def json_processing(json_file, company_file):
+    file = open(json_file)
+    json_str = file.read()
+    file.close()
+    json_data = json.loads(json_str)
+    company_names = get_company_names(company_file)
+    company_json = {}
+    for name in company_names:
+        company_json[name] = []
+    for line in json_data:
+        for name in company_names:
+            if name in line['query']:
+                company_json[name].append(line)
+                break
+    return company_json
+
+
+def query_processing(company_json):
+    company_querys = {}
+    for company in company_json.keys():
+        company_querys[company] = []
+    for company, querys in company_json.items():
+        for query in querys:
+            company_querys[company].append(SearchQuery(company, query))
+    return company_querys
+
+def remove_irrelevant_urls(company_querys):
+    company_all_urls = {}
+    for company, querys in company_querys.items():
+        company_all_urls[company] = []
+        for query in querys:
+            for result in query.results:
+                if result in company_all_urls[company]:
+                    pass
+                else:
+                    company_all_urls[company].append(result)
+    return company_all_urls
+
+
+company_json = json_processing('/Users/keleigong/Dropbox/Python/AUTO_Rating/URLExtraction/concinnity_600/1-50.json',
+                               '/Users/keleigong/Dropbox/Python/AUTO_Rating/URLExtraction/concinnity_600/1-50')
+company_querys = query_processing(company_json)
+
+company_all_urls = remove_irrelevant_urls(company_querys)
+# print(get_company_names('/Users/keleigong/Dropbox/Python/AUTO_Rating/URLExtraction/concinnity_600/1-50'))
 # company = {}
 # count = {}
 # for row in ws.rows:
