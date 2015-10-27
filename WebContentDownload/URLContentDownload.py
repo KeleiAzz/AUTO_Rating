61948465749314ca5064f150cd7283dd898fe996
Kelei Gong <kgong@ncsu.edu>
2015-10-26 22:30:51 -0400

    iterate throught all urls

--- WebContentDownload/URLContentDownload.py
+++ WebContentDownload/URLContentDownload.py
@@ -1,24 +1,5 @@
 __author__ = 'keleigong'
 
-from selenium import webdriver
-
-import os
-# profile = webdriver.FirefoxProfile()
-# profile.set_preference('browser.download.folderList', 2)
-# profile.set_preference('browser.download.manager.showWhenStarting', False)
-# profile.set_preference('browser.download.dir', os.getcwd())
-# profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/pdf')
-#
-#
-# profile.set_preference('pdfjs.disabled', True)
-#
-# profile.set_preference("plugin.scan.plid.all", False)
-# profile.set_preference("plugin.scan.Acrobat", "99.0")
-#
-# driver = webdriver.Firefox(profile)
-# driver.get('http://anh.cs.luc.edu/python/hands-on/3.1/Hands-onPythonTutorial.pdf')
-# html = driver.page_source
-
 import html2text
 import urllib.request
 import URLExtraction.URLsResultsProcessing as URL
@@ -29,11 +10,6 @@ company_querys = URL.query_processing(company_json)
 
 company_all_urls = URL.remove_irrelevant_urls(company_querys)
 
-urls = [result.link for result in company_all_urls['BIOGEN']]
-# urls =['https://www.bankofamerica.com/suppliers/supply-chain-management.go?request_locale=en_US',
-#       'http://about.bankofamerica.com/en-us/our-story/supplier-relations.html',
-#       'http://about.bankofamerica.com/en-us/our-story/supplier-diversity.html',
-#       'http://www.bofaml.com/en-us/content/trade-supply-chain.html']
 
 # with urllib.request.urlopen(url) as response:
 #     html = response.read()
@@ -58,6 +34,7 @@ class Fetcher:
                         'Connection': 'keep-alive'}
         self.opener = urllib2.build_opener(urllib2.HTTPHandler)
         # self.opener.addheaders(self.header)
+        # self.h = html2text.HTML2Text()
         self.lock = Lock() #线程锁
         self.q_req = Queue() #任务队列
         self.q_ans = Queue() #完成队列
@@ -77,8 +54,8 @@ class Fetcher:
     def taskleft(self):
         return self.q_req.qsize()+self.q_ans.qsize()+self.running
 
-    def push(self,req):
-        req = urllib2.Request(req, headers=self.header)
+    def push(self, req):
+        req = (req[0], urllib2.Request(req[1], headers=self.header))
         self.q_req.put(req)
 
     def pop(self):
@@ -90,37 +67,23 @@ class Fetcher:
             with self.lock: #要保证该操作的原子性，进入critical area
                 self.running += 1
             try:
-                ans = self.opener.open(req, timeout=5)
+                ans = self.opener.open(req[1], timeout=5)
                 if ans.getheader('Content-Type') == 'application/pdf':
-                    file = open(str(self.pdfcounter) + '.pdf', 'wb')
-                    self.pdfcounter += 1
-                    file.write(ans.read())
-                    file.close()
+                    # file = open( req[0] + str(self.pdfcounter) + '.pdf', 'wb')
+                    with self.lock:
+                        self.pdfcounter += 1
+                    # file.write(ans.read())
+                    # file.close()
                     print('PDF downloaded')
                     ans = 'PDF content'
                 else:
-                    ans = ans.read()
-                # print(ans.getheader('Content-Type'))  # application/pdf
-                # ans = ans.read()
+                    h = html2text.HTML2Text()
+                    ans = h.handle(ans.read().decode('ISO-8859-1'))
+                    # ans = (req[0], ans)
+                    # ans = ans.read()
             except Exception as what:
                 ans = ''
                 print(what)
-            # if ans == '':
-            #     self.q_ans.put((req,'no content fetched'))
-            #     with self.lock:
-            #         self.running -= 1
-            # elif ans.getheader('Content-Type') == 'application/pdf':
-            #     self.q_ans.put((req,'pdf content'))
-            #     self.pdfcounter += 1
-            #     # tmp = ans.read()
-            #     file = open(str(self.pdfcounter) + '.pdf', 'wb')
-            #     file.write(ans.read())
-            #     file.close()
-            #     print("PDF downloaded")
-            #     with self.lock:
-            #         self.running -= 1
-            # else:
-            #     ans = ans.read()
             self.q_ans.put((req, ans))
             with self.lock:
                 self.running -= 1
@@ -129,11 +92,18 @@ class Fetcher:
 
 # if __name__ == "__main__":
 #     links = [ 'http://www.verycd.com/topics/%d/'%i for i in range(5420,5430) ]
+urls = []
+for company, results in company_all_urls.items():
+    for result in results:
+        urls.append((company, result.link))
+# urls = [result.link for result in company_all_urls['BIOGEN']]
 f = Fetcher(threads=10)
+h = html2text.HTML2Text()
 for url in urls:
     f.push(url)
 while f.taskleft():
     url, content = f.pop()
-    print(url.get_full_url(), len(content))
+    print(url[0], url[1].get_full_url(), len(content))
+    # print(len(h.handle(content.decode('ISO-8859-1'))))
 
 # a = urllib2
