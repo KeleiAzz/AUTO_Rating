180646221a0d453e261214c8a3b5e43b1ae0f8c1
Kelei Gong <kgong@ncsu.edu>
2015-10-30 10:18:43 -0400

    use pypdf to convert pdf to text

--- WebContentDownload/pdf2text.py
+++ WebContentDownload/pdf2text.py
@@ -7,8 +7,22 @@ from pdfminer.layout import LAParams
 def to_txt(pdf_path):
     input_ = open(pdf_path, 'rb')
     output = StringIO()
-
     manager = PDFResourceManager()
     converter = TextConverter(manager, output, laparams=LAParams())
     process_pdf(manager, converter, input_)
     return output.getvalue()
+
+# import PyPDF2
+from PyPDF2 import PdfFileReader
+
+def to_text2(pdf_path):
+    input = open(pdf_path, 'rb')
+    pdfReader = PdfFileReader(input)
+    # output = StringIO()
+    text = ''
+    for i in range(pdfReader.getNumPages()):
+        text += '\n' + pdfReader.getPage(i).extractText()
+    return text
+    # pdfReader.read()
+
+
